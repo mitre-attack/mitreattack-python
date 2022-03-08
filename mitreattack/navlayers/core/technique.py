07c570c437d2ca8fc475d03b1c82c43103ce5ae3
Caleb R Little <CLITTLE@MITRE.ORG>
2022-03-08 13:06:45 -0500

    Adds support for pre-initialized Metadata, MetaDiv, Link and LinkDiv objects

--- mitreattack/navlayers/core/technique.py
+++ mitreattack/navlayers/core/technique.py
@@ -107,6 +107,12 @@ class Technique:
                     if isinstance(entry, Metadata) or isinstance(entry, MetaDiv):
                         loadChecker(type(self).__name__, entry.get_dict(), ['name', 'value'], "metadata")
                         self.__metadata.append(entry)
+                    elif isinstance(entry, dict):
+                        loadChecker(type(self).__name__, entry, ['name', 'value'], "metadata")
+                        if entry['name'] == "DIVIDER":
+                            self.__metadata.append(MetaDiv(active=entry['value']))
+                        else:
+                            self.__metadata.append(Metadata(name=entry['name'], value=entry['value']))
                     else:
                         pass  # Object in the list was not of Metadata or MetaDiv classes
                 except MissingParameters as e:
@@ -153,10 +159,21 @@ class Technique:
         entry = ""
         try:
             for entry in links:
-                if "divider" in entry:
-                    self.__links.append(Link(entry["divider"]))
+                if isinstance(entry, Link):
+                    loadChecker(type(self).__name__, entry.get_dict(), ['label', 'url'], "link")
+                    self.__links.append(entry)
+                elif isinstance(entry, LinkDiv):
+                    loadChecker(type(self).__name__, entry.get_dict(), ['name', 'value'], "linkdiv")
+                    self.__links.append(entry)
+                elif isinstance(entry, dict):
+                    if 'name' in entry and entry['name'] == "DIVIDER":
+                        loadChecker(type(self).__name__, entry, ['name', 'value'], "linkdiv")
+                        self.__links.append(LinkDiv(active=entry['value']))
+                    else:
+                        loadChecker(type(self).__name__, entry, ['label', 'url'], "link")
+                        self.__links.append(Link(label=entry['label'], url=entry['url']))
                 else:
-                    self.__links.append(Link(entry['label'], entry['url']))
+                    pass
         except KeyError as e:
             handler(type(self).__name__, 'Link {} is missing parameters: '
                                          '{}. Unable to load.'
