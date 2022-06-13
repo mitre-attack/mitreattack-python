1700878781cc44b8845491cdf9cb2d654b3a313a
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-06-13 08:42:07 -0400

    decoupled ATT&CK ID source names; migrated 'source of truth' to constants.py

---
+++ mitreattack/constants.py
@@ -0,0 +1,3 @@
+# The ATT&CK ID entry always has a source_name with a value from:
+# ['mitre-attack', 'mitre-mobile-attack', 'mobile-attack', 'mitre-ics-attack']
+MITRE_ATTACK_ID_SOURCE_NAMES = ['mitre-attack', 'mobile-attack', 'mitre-mobile-attack', 'mitre-ics-attack']
