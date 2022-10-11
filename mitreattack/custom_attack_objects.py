b69a30154472ac4d6918dac1744f0ff60ab06b43
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2022-10-11 14:59:52 -0400

    custom attack object definitions

---
+++ mitreattack/custom_attack_objects.py
@@ -0,0 +1,108 @@
+from stix2 import CustomObject, ExternalReference
+from stix2.properties import StringProperty, ListProperty, TypeProperty, IDProperty, ReferenceProperty, TimestampProperty, BooleanProperty
+
+class StixObject(object):
+    def get_version(self) -> str:
+        """Get the version of the object
+
+        Returns
+        -------
+        str
+            the object version
+        """
+        return self.x_mitre_version
+
+@CustomObject('x-mitre-matrix', [
+    # SDO Common Properties
+    ('id', IDProperty('x-mitre-matrix', spec_version='2.0')),
+    ('type', TypeProperty('x-mitre-matrix', spec_version='2.0')),
+    ('created_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('created', TimestampProperty(precision='millisecond')),
+    ('modified', TimestampProperty(precision='millisecond')),
+    ('revoked', BooleanProperty(default=lambda: False)),
+    ('external_references', ListProperty(ExternalReference)),
+    ('object_marking_refs', ListProperty(ReferenceProperty(valid_types='marking-definition', spec_version='2.0'))),
+    # Matrix Properties
+    ('name', StringProperty()),
+    ('description', StringProperty()),
+    ('tactic_refs', ListProperty(ReferenceProperty(valid_types='x-mitre-tactic', spec_version='2.0'))),
+    ('x_mitre_modified_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('x_mitre_version', StringProperty()),
+    ('x_mitre_attack_spec_version', StringProperty())
+])
+class Matrix(StixObject, object):
+    pass
+
+@CustomObject('x-mitre-tactic', [
+    # SDO Common Properties
+    ('id', IDProperty('x-mitre-tactic', spec_version='2.0')),
+    ('type', TypeProperty('x-mitre-tactic', spec_version='2.0')),
+    ('created_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('created', TimestampProperty(precision='millisecond')),
+    ('modified', TimestampProperty(precision='millisecond')),
+    ('revoked', BooleanProperty(default=lambda: False)),
+    ('external_references', ListProperty(ExternalReference)),
+    ('object_marking_refs', ListProperty(ReferenceProperty(valid_types='marking-definition', spec_version='2.0'))),
+    # Tactic Properties
+    ('name', StringProperty()),
+    ('description', StringProperty()),
+    ('x_mitre_domains', ListProperty(StringProperty())),
+    ('x_mitre_shortname', StringProperty()),
+    ('x_mitre_version', StringProperty()),
+    ('x_mitre_attack_spec_version', StringProperty())
+])
+class Tactic(StixObject, object):
+    def get_shortname(self) -> str:
+        """Get the tactic shortname
+
+        Returns
+        -------
+        str
+            the shortname of the tactic
+        """
+        return self.x_mitre_shortname
+
+@CustomObject('x-mitre-data-source', [
+    # SDO Common Properties
+    ('id', IDProperty('x-mitre-data-source', spec_version='2.0')),
+    ('type', TypeProperty('x-mitre-data-source', spec_version='2.0')),
+    ('created_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('created', TimestampProperty(precision='millisecond')),
+    ('modified', TimestampProperty(precision='millisecond')),
+    ('revoked', BooleanProperty(default=lambda: False)),
+    ('external_references', ListProperty(ExternalReference)),
+    ('object_marking_refs', ListProperty(ReferenceProperty(valid_types='marking-definition', spec_version='2.0'))),
+    # Data Source Properties
+    ('name', StringProperty()),
+    ('description', StringProperty()),
+    ('x_mitre_platforms', ListProperty(StringProperty())),
+    ('x_mitre_domains', ListProperty(StringProperty())),
+    ('x_mitre_collection_layers', ListProperty(StringProperty())),
+    ('x_mitre_contributors', ListProperty(StringProperty())),
+    ('x_mitre_modified_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('x_mitre_version', StringProperty()),
+    ('x_mitre_attack_spec_version', StringProperty())
+])
+class DataSource(StixObject, object):
+    pass
+
+@CustomObject('x-mitre-data-component', [
+    # SDO Common Properties
+    ('id', IDProperty('x-mitre-data-component', spec_version='2.0')),
+    ('type', TypeProperty('x-mitre-data-component', spec_version='2.0')),
+    ('created_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('created', TimestampProperty(precision='millisecond')),
+    ('modified', TimestampProperty(precision='millisecond')),
+    ('revoked', BooleanProperty(default=lambda: False)),
+    ('external_references', ListProperty(ExternalReference)),
+    ('object_marking_refs', ListProperty(ReferenceProperty(valid_types='marking-definition', spec_version='2.0'))),
+    # Data Component Properties
+    ('name', StringProperty()),
+    ('description', StringProperty()),
+    ('x_mitre_data_source_ref', ReferenceProperty(valid_types='x-mitre-data-source', spec_version='2.0')),
+    ('x_mitre_modified_by_ref', ReferenceProperty(valid_types='identity', spec_version='2.0')),
+    ('x_mitre_version', StringProperty()),
+    ('x_mitre_attack_spec_version', StringProperty())
+])
+class DataComponent(StixObject, object):
+    pass
