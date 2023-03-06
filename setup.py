4e5b03883ac6c5fde30be160a37f4defa0d5ea60
Jared Ondricek <jondricek@mitre.org>
2023-03-06 14:28:53 -0600

    Update to 2.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mitreattack-python",
-    version="2.0.4",
+    version="2.0.5",
     author="MITRE ATT&CK, MITRE Corporation",
     author_email="attack@mitre.org",
     description="MITRE ATT&CK python library",
