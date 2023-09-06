3227bbfaa041883ad3143655b2b2669a895336a4
Sun <vsun@mitre.org>
2023-09-06 14:30:09 -0400

    all code examples work

--- examples/get_campaign_by_alias.py
+++ examples/get_campaign_by_alias.py
@@ -4,9 +4,9 @@ from mitreattack.stix20 import MitreAttackData
 def main():
     mitre_attack_data = MitreAttackData("enterprise-attack.json")
 
-    C0001 = mitre_attack_data.get_campaign_by_alias("Frankenstein")
+    C0001 = mitre_attack_data.get_campaigns_by_alias("Frankenstein")
 
-    print(C0001.serialize(pretty=True))
+    print(C0001[0].serialize(pretty=True))
 
 
 if __name__ == "__main__":
